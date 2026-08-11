21798eb4df099f975c4fa40e19449d2c06ba14d4
Arthur He <arthurhe2016@gmail.com>
2026-08-10 23:57:39 -0400

    plot for BMI x ever-drinking interaction - Namba S chr 1 loads, (didnt do more because takes longer

--- manhattanly/manhattanlyTest.R
+++ manhattanly/manhattanlyTest.R
@@ -1,17 +1,24 @@
 # testing manhattanly funcitons with HapMap
 
 library(manhattanly)
-manhattanly(HapMap)
+# manhattanly(HapMap)
 
 set.seed(12345)
-HapMap.subset <- subset(HapMap, CHR %in% 4:7)
+# HapMap.subset <- subset(HapMap, CHR %in% 4:7)
 # for highlighting SNPs of interest
-significantSNP <- sample(HapMap.subset$SNP, 20)
+# significantSNP <- sample(HapMap.subset$SNP, 20)
 
-df <- subset(HapMap, CHR %in% 1:3)
+# df <- subset(HapMap, CHR %in% 1:3)
 
-head(HapMap.subset)
+# head(HapMap.subset)
 
-dim(HapMap.subset)
+# dim(HapMap.subset)
 
-manhattanly(HapMap.subset, snp = "SNP", gene = "GENE")
+# manhattanly(HapMap.subset, snp = "SNP", gene = "GENE")
+
+testr <- read.csv("C:/Users/arthu/Documents/Research/localGWASdata/GCST90681941.h.tsv/GCST90681941.h.tsv", sep = "\t")
+testr.chr1to2 <- subset(testr, chromosome %in% 1:2)
+testr.chr1 <- subset(testr, chromosome %in% 1)
+head(testr.chr1)
+dim(testr.chr1)
+manhattanly(testr.chr1, chr = "chromosome", snp = "rsid", bp = "base_pair_location", p = "p_value")
