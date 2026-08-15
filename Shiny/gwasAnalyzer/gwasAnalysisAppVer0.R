1a614a8dc2c2b7c4390b34b4fd00cb7cfabca2c8
Arthur He <arthurhe2016@gmail.com>
2026-08-15 11:46:46 -0400

    push all changes

--- Shiny/gwasAnalyzer/gwasAnalysisAppVer0.R
+++ Shiny/gwasAnalyzer/gwasAnalysisAppVer0.R
@@ -11,14 +11,25 @@ library(plotly)
 #library(palmerpenguins) #xtra
 #library(dplyr) #xtra
 
-data("airquality")
+# bmixdrinking <- read.csv("C:/Users/arthu/Documents/Research/localGWASdata/GCST90681941.h.tsv/GCST90681941.h.tsv", sep = "\t")
 
-bmixdrinking <- read.csv("C:/Users/arthu/Documents/Research/localGWASdata/GCST90681941.h.tsv/GCST90681941.h.tsv", sep = "\t")
+# fread
 
-bchr1 <- subset(bmixdrinking, chromosome %in% 1)
+# bchr1 <- subset(bmixdrinking, chromosome %in% 1)
 
-bchr1ovrp5 <- subset(bchr1, p_value < 1e-5)
+# bchr1ovrp5 <- subset(bchr1, p_value < 1e-5)
 
+# sys1 <- read.csv("C:/Users/arthu/Documents/Research/localGWASdata/gwasTestSystems/gwas_sys1.B1.glm.logistic", sep = "\t")
+
+# sys1chr1 <- subset(sys1, CHROM %in% 1)
+
+# sys1chr1ovrp5 <- subset(sys1chr1, P < 1e-5)
+
+#sys2 <- read.csv("C:/Users/arthu/Documents/Research/localGWASdata/gwasTestSystems/gwas_sys2.B1.glm.logistic", sep = "\t")
+
+#sys2chr1 <- subset(sys2, CHROM %in% 1)
+
+#sys2chr1ovrp5 <- subset(sys2chr1, P < 1e-5)
 
 # Define UI
 ui <- fluidPage(theme = shinytheme("cerulean"),
@@ -46,11 +57,13 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                            
                            mainPanel(
                              # Use plotlyOutput to handle the htmlwidget
-                             plotlyOutput("manhattanPlot", height = "600px")
+                             plotlyOutput("manhattanPlot1", height = "600px"),
+                             plotlyOutput("manhattanPlot2", height = "600px")
                            )
                   ),
                   tabPanel("Navbar 3", 
-                           dataTableOutput("table"),
+                           dataTableOutput("table1"),
+                           dataTableOutput("table2"),
                            "Hello")
                   
                   
@@ -64,17 +77,28 @@ server <- function(input, output) {
   output$txtout <- renderText({
     paste( input$txt1, input$txt2, sep = " " )
   })
-  output$manhattanPlot <- renderPlotly({
+  output$manhattanPlot1 <- renderPlotly({
     # Use renderPlotly to wrap the manhattanly function call
     manhattanly(
-      bchr1ovrp5,          # Swap with your actual dataset variable
-      chr = "chromosome",
-      bp = "base_pair_location",
-      p = "p_value",
-      snp = "rsid"
+      sys1chr1ovrp5,          # Swap with your actual dataset variable
+      chr = "CHROM",
+      bp = "POS",
+      p = "P",
+      snp = "ID"
     )
   })
-  output$table <- renderDataTable({datatable(bchr1ovrp5)})
+  output$manhattanPlot2 <- renderPlotly({
+    # Use renderPlotly to wrap the manhattanly function call
+    manhattanly(
+      sys2chr1ovrp5,          # Swap with your actual dataset variable
+      chr = "CHROM",
+      bp = "POS",
+      p = "P",
+      snp = "ID"
+    )
+  })
+  output$table1 <- renderDataTable({datatable(sys1chr1ovrp5)})
+  output$table2 <- renderDataTable({datatable(sys2chr1ovrp5)})
 } # server
 
 
