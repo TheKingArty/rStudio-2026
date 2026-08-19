f653e9ec8351367253469aedb40e9f6ed9e2c31d
Arthur He <arthurhe2016@gmail.com>
2026-08-19 12:43:19 -0400

    input works now

--- Shiny/gwasAnalyzer/gwasAnalysisAppVer0.R
+++ Shiny/gwasAnalyzer/gwasAnalysisAppVer0.R
@@ -8,6 +8,10 @@ library(manhattanly)
 library(data.table)
 library(plotly)
 
+#set max upload size 1GB
+
+options(shiny.maxRequestSize = 1000 * 1024^2)
+
 #library(bslib) #xtra
 #library(palmerpenguins) #xtra
 #library(dplyr) #xtra
@@ -55,17 +59,32 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                   ), # Navbar 1, tabPanel
                   tabPanel("Navbar 2",
                            titlePanel("Interactive GWAS Manhattan Plot"),
+                           fluidRow(
+                             column(
+                               width = 6,
+                               fileInput("sys1_file", "Upload Sys 1 Dataset", accept = c(".tsv", ".logistic", ".txt", ".csv"))
+                             ),
+                             column(
+                               width = 6,
+                               fileInput("sys2_file", "Upload Sys 2 Dataset", accept = c(".tsv", ".logistic", ".txt", ".csv"))
+                             )
+                           ),
+                           hr(), #seperator
+                           
                            
                            fluidRow(
                              column(
                                width = 6,
-                               plotlyOutput("manhattanPlot1", height = "600px")
+                               plotlyOutput("manhattanPlot1",
+                                            height = "600px")
                              ),
                              column(
                                width = 6,
-                               plotlyOutput("manhattanPlot2", height = "600px")
+                               plotlyOutput("manhattanPlot2",
+                                            height = "600px")
                              )
                            )
+                           
                   ),
                   tabPanel("Navbar 3", 
                            dataTableOutput("table1"),
@@ -77,16 +96,38 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
 ) # fluidPage
 
 
+
 # Define server function  
 server <- function(input, output) {
   
   output$txtout <- renderText({
     paste( input$txt1, input$txt2, sep = " " )
   })
+  
+  # Process input 1
+  
+  inp1 <- reactive({
+    req(input$sys1_file) # Waits until user uploads a file
+    
+    sys1 <- fread(input$sys1_file$datapath, sep = "\t")
+    sys1chr1 <- subset(sys1, CHROM %in% 1)
+    subset(sys1chr1, P < 1e-5)
+  })
+  
+  # process input 2
+  
+  inp2 <- reactive({
+    req(input$sys2_file) # Waits until user uploads a file
+    
+    sys2 <- fread(input$sys2_file$datapath, sep = "\t")
+    sys2chr1 <- subset(sys2, CHROM %in% 1)
+    subset(sys2chr1, P < 1e-5)
+  })
+  
   output$manhattanPlot1 <- renderPlotly({
     # Use renderPlotly to wrap the manhattanly function call
     manhattanly(
-      sys1chr1ovrp5,          # Swap with your actual dataset variable
+      inp1(),          # Swap with your actual dataset variable
       chr = "CHROM",
       bp = "POS",
       p = "P",
@@ -96,7 +137,7 @@ server <- function(input, output) {
   output$manhattanPlot2 <- renderPlotly({
     # Use renderPlotly to wrap the manhattanly function call
     manhattanly(
-      sys2chr1ovrp5,          # Swap with your actual dataset variable
+      inp2(),          # Swap with your actual dataset variable
       chr = "CHROM",
       bp = "POS",
       p = "P",
