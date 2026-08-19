1911b0a9fa55eb69f6dba3883636b4a31cb03e63
Arthur He <arthurhe2016@gmail.com>
2026-08-19 12:25:28 -0400

    changed readcsv to fread and made grpahs next to each other

--- Shiny/gwasAnalyzer/gwasAnalysisAppVer0.R
+++ Shiny/gwasAnalyzer/gwasAnalysisAppVer0.R
@@ -5,31 +5,32 @@ library(shiny) # shiny
 library(shinythemes) # themes
 library(DT) # datatables
 library(manhattanly)
+library(data.table)
 library(plotly)
 
 #library(bslib) #xtra
 #library(palmerpenguins) #xtra
 #library(dplyr) #xtra
 
-# bmixdrinking <- read.csv("C:/Users/arthu/Documents/Research/localGWASdata/GCST90681941.h.tsv/GCST90681941.h.tsv", sep = "\t")
+#bmixdrinking <- read.csv("C:/Users/arthu/Documents/Research/localGWASdata/GCST90681941.h.tsv/GCST90681941.h.tsv", sep = "\t")
 
 # fread
 
-# bchr1 <- subset(bmixdrinking, chromosome %in% 1)
+#bchr1 <- subset(bmixdrinking, chromosome %in% 1)
 
-# bchr1ovrp5 <- subset(bchr1, p_value < 1e-5)
+#bchr1ovrp5 <- subset(bchr1, p_value < 1e-5)
 
-# sys1 <- read.csv("C:/Users/arthu/Documents/Research/localGWASdata/gwasTestSystems/gwas_sys1.B1.glm.logistic", sep = "\t")
+sys1 <- fread("C:/Users/arthu/Documents/Research/localGWASdata/gwasTestSystems/gwas_sys1.B1.glm.logistic", sep = "\t")
 
-# sys1chr1 <- subset(sys1, CHROM %in% 1)
+sys1chr1 <- subset(sys1, CHROM %in% 1)
 
-# sys1chr1ovrp5 <- subset(sys1chr1, P < 1e-5)
+sys1chr1ovrp5 <- subset(sys1chr1, P < 1e-5)
 
-#sys2 <- read.csv("C:/Users/arthu/Documents/Research/localGWASdata/gwasTestSystems/gwas_sys2.B1.glm.logistic", sep = "\t")
+sys2 <- fread("C:/Users/arthu/Documents/Research/localGWASdata/gwasTestSystems/gwas_sys2.B1.glm.logistic", sep = "\t")
 
-#sys2chr1 <- subset(sys2, CHROM %in% 1)
+sys2chr1 <- subset(sys2, CHROM %in% 1)
 
-#sys2chr1ovrp5 <- subset(sys2chr1, P < 1e-5)
+sys2chr1ovrp5 <- subset(sys2chr1, P < 1e-5)
 
 # Define UI
 ui <- fluidPage(theme = shinytheme("cerulean"),
@@ -55,10 +56,15 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                   tabPanel("Navbar 2",
                            titlePanel("Interactive GWAS Manhattan Plot"),
                            
-                           mainPanel(
-                             # Use plotlyOutput to handle the htmlwidget
-                             plotlyOutput("manhattanPlot1", height = "600px"),
-                             plotlyOutput("manhattanPlot2", height = "600px")
+                           fluidRow(
+                             column(
+                               width = 6,
+                               plotlyOutput("manhattanPlot1", height = "600px")
+                             ),
+                             column(
+                               width = 6,
+                               plotlyOutput("manhattanPlot2", height = "600px")
+                             )
                            )
                   ),
                   tabPanel("Navbar 3", 
